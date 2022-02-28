24bd8a281ab6f3b589a9a526d04f6742a843d2e3
VirgiRom <virginiaromero.mail@gmail.com>
2022-02-28 17:57:20 -0300

    ejercicio completo

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-alert('Hola Mundo');
+flexRadioDefault1.addEventListener('click', function () {
+    basico.innerHTML = "<a>: $500</a>";
+    
+});
+
+flexRadioDefault2.addEventListener('click', function () {
+    intermedio.innerHTML = "<a>: $1000</a>";
+    
+});
+
+flexRadioDefault3.addEventListener('click', function () {
+    premium.innerHTML = "<a>: $1500</a>";
+    
+});
